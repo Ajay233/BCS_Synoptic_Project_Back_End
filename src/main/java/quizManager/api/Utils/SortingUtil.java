08b476e6d01f50528bf42baafafbbf97f12680c2
Ajay <ajaymungur@hotmail.com>
2020-06-23 09:19:13 +0100

    Created sortign util to perform sequence sort on Questions and answers

--- src/main/java/quizManager/api/Utils/SortingUtil.java
+++ src/main/java/quizManager/api/Utils/SortingUtil.java
@@ -1,6 +1,7 @@
 package quizManager.api.Utils;
 
 import org.springframework.stereotype.Service;
+import quizManager.api.Answer.Answer;
 import quizManager.api.Question.Question;
 
 import java.util.List;
@@ -17,8 +18,39 @@ public class SortingUtil {
             int indexOfSmallestVal = i;
 
             for(int x = i; x < highestIndex; x++){
-                
+                if(smallestVal > questions.get(x + 1).getQuestionNumber()){
+                    smallestVal = questions.get(x + 1).getQuestionNumber();
+                    indexOfSmallestVal = x + 1;
+                }
             }
+
+            Question startingVal = questions.get(i);
+            Question valToSwap = questions.get(indexOfSmallestVal);
+            questions.set(i, valToSwap);
+            questions.set(indexOfSmallestVal, startingVal);
+
+        }
+    }
+
+    public void answerSelectSort(List<Answer> answers, int listSize){
+        int highestIndex = listSize -1;
+
+        for(int i = 0; i < highestIndex; i++){
+
+            String smallestVal = answers.get(i).getAnswerIndex();
+            int indexOfSmallestVal = i;
+
+            for(int x = i; x < highestIndex; x++){
+                if(smallestVal.compareTo(answers.get(x + 1).getAnswerIndex()) == 1){
+                    smallestVal = answers.get(x + 1).getAnswerIndex();
+                    indexOfSmallestVal = x + 1;
+                }
+            }
+
+            Answer startingVal = answers.get(i);
+            Answer valToSwap = answers.get(indexOfSmallestVal);
+            answers.set(i, valToSwap);
+            answers.set(indexOfSmallestVal, startingVal);
 
         }
     }
